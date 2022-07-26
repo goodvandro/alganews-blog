45044cd570bc3d7175d8b7ccf59b8fdd1e9e545e
Evandro Lima <evandro.lima@gat.digital>
2022-07-26 17:20:58 +0000

    Add image fallback with nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import FeaturedPost from '../components/FeaturedPost'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -21,6 +22,45 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+
+        <FeaturedPost postSummary={{
+          id: 42,
+          slug: "como-fazer-x-coisas-com-react-js",
+          title: "Como fazer X coisas com React.js",
+          imageUrls: {
+            default:
+              "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
+            small:
+              "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
+            medium:
+              "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
+            large:
+              "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
+          },
+          editor: {
+            id: 29,
+            name: "Daniel Bonifacio",
+            avatarUrls: {
+              default:
+                "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
+              small:
+                "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
+              medium:
+                "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
+              large:
+                "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
+            },
+            createdAt: "2017-03-04T00:12:45Z",
+          },
+          createdAt: "2020-12-04T00:12:45-03:00",
+          updatedAt: "2020-12-05T00:12:45-03:00",
+          published: true,
+          tags: ["JavaScript", "react"],
+          canBePublished: true,
+          canBeUnpublished: true,
+          canBeDeleted: true,
+          canBeEdited: true,
+        }} />
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
