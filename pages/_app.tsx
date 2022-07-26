7cec99f3f2231d6f2b1f2319b8fee06b0842f2dc
Evandro Lima <evandro.lima@gat.digital>
2022-07-26 03:03:40 +0000

    Add theme + global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { light } from '../styles/theme'
+import GlobalStyles from '../styles/globalStyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={light}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
