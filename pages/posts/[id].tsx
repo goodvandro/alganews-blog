202f1835730be39af7087beff099b4c7e6438c9c
Evandro Lima <evandro.lima@gat.digital>
2022-07-29 12:46:45 +0000

    Add post page detail

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,35 @@
+import { Post, PostService } from "goodvandro-alganews-sdk"
+import { GetServerSideProps } from "next"
+import { ParsedUrlQuery } from "querystring"
+
+interface PostProps {
+  post: Post.Detailed
+}
+
+export default function PostPage(props: PostProps) {
+  return (
+    <div> {props.post.title} </div>
+  )
+}
+
+interface Params extends ParsedUrlQuery {
+  id: string
+}
+
+export const getServerSideProps: GetServerSideProps<PostProps, Params> =
+  async ({ params }) => {
+    if (!params) return { notFound: true }
+
+    const { id } = params
+    const postId = Number(id)
+
+    if (isNaN(postId)) return { notFound: true }
+
+    const post = await PostService.getExistingPost(postId)
+
+    return {
+      props: {
+        post,
+      },
+    }
+  }
