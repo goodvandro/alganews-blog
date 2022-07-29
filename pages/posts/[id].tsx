18e21d8b955aad2cfe8649f81a651e9154fd4086
Evandro Lima <evandro.lima@gat.digital>
2022-07-29 12:51:25 +0000

    Capture exception with try catch

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,12 +3,12 @@ import { GetServerSideProps } from "next"
 import { ParsedUrlQuery } from "querystring"
 
 interface PostProps {
-  post: Post.Detailed
+  post?: Post.Detailed
 }
 
 export default function PostPage(props: PostProps) {
   return (
-    <div> {props.post.title} </div>
+    <div> {props.post?.title} </div>
   )
 }
 
@@ -18,18 +18,24 @@ interface Params extends ParsedUrlQuery {
 
 export const getServerSideProps: GetServerSideProps<PostProps, Params> =
   async ({ params }) => {
-    if (!params) return { notFound: true }
+    try {
+      if (!params) return { notFound: true }
 
-    const { id } = params
-    const postId = Number(id)
+      const { id } = params
+      const postId = Number(id)
 
-    if (isNaN(postId)) return { notFound: true }
+      if (isNaN(postId)) return { notFound: true }
 
-    const post = await PostService.getExistingPost(postId)
+      const post = await PostService.getExistingPost(postId)
 
-    return {
-      props: {
-        post,
-      },
+      return {
+        props: {
+          post,
+        },
+      }
+    } catch (err) {
+      return {
+        props: {},
+      }
     }
   }
