4118ce52eb5dcedce150d23d61f93c1df74ec946
Evandro Lima <evandro.lima@gat.digital>
2022-07-29 11:24:35 +0000

    Make display posts responsive

--- components/PostsGrid.tsx
+++ components/PostsGrid.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export default styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
   gap: 16px;
+  
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `
