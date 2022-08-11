4a322ffb00bee68481ebfb7dcfe5fcefc00c4125
Evandro Lima <evandro.lima@gat.digital>
2022-08-11 15:01:14 +0000

    Make menu responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 import { transparentize } from "polished"
-import { HEADER_HEIGHT } from "../_constants"
+import { HEADER_HEIGHT, MOBILE_HEADER_HEIGHT } from "../_constants"
 import Logo from "./Logo"
 import NavBar from "./NavBar"
 
@@ -22,6 +22,10 @@ const Wrapper = styled.div`
 
   width: 100%;
   height: ${HEADER_HEIGHT}px;
+
+  @media screen and (max-width: 767px) {
+      height: ${MOBILE_HEADER_HEIGHT}px;
+  }
 `
 
 const Container = styled.div`
@@ -33,4 +37,10 @@ const Container = styled.div`
   height: 100%;
 
   padding: 0 16px;
+
+  @media screen and (max-width: 767px) {
+    flex-direction: column;
+    justify-content: center;
+    gap: 12px;
+  }
 `
